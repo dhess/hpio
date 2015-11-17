c560aa9d6fb265141652ea43f83d20563fc92b0b
Drew Hess <src@drewhess.com>
2015-11-16 16:47:04 -1000

    Add errors and exceptions packages.
    Not using these yet, but I expect to.

--- default.nix
+++ default.nix
@@ -1,10 +1,11 @@
-{ mkDerivation, base, free, hspec, stdenv }:
+{ mkDerivation, base, errors, exceptions, free, hspec, mtl, stdenv
+}:
 mkDerivation {
   pname = "gpio";
   version = "0.0.0";
   src = ./.;
-  libraryHaskellDepends = [ base free ];
-  testHaskellDepends = [ base free hspec ];
+  libraryHaskellDepends = [ base errors exceptions free mtl ];
+  testHaskellDepends = [ base errors exceptions free hspec mtl ];
   homepage = "https://github.com/dhess/gpio";
   description = "Control GPIO pins";
   license = stdenv.lib.licenses.bsd3;
--- shell.nix
+++ shell.nix
@@ -4,13 +4,15 @@ let
 
   inherit (nixpkgs) pkgs;
 
-  f = { mkDerivation, base, free, hspec, stdenv }:
+  f = { mkDerivation, base, errors, exceptions, free, hspec, mtl
+      , stdenv
+      }:
       mkDerivation {
         pname = "gpio";
         version = "0.0.0";
         src = ./.;
-        libraryHaskellDepends = [ base free ];
-        testHaskellDepends = [ base free hspec ];
+        libraryHaskellDepends = [ base errors exceptions free mtl ];
+        testHaskellDepends = [ base errors exceptions free hspec mtl ];
         homepage = "https://github.com/dhess/gpio";
         description = "Control GPIO pins";
         license = stdenv.lib.licenses.bsd3;
