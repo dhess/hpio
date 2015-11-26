197b26927b6ce51a1ab48b355f3b5ed20692fa08
Drew Hess <src@drewhess.com>
2015-11-26 09:35:32 -0800

    Test cleanup.

--- test/System/GPIOSpec.hs
+++ test/System/GPIOSpec.hs
@@ -10,21 +10,19 @@ import System.GPIO.Mock
 
 import Test.Hspec
 
-testOpenClose :: (MonadError String m) => GpioT m String
+testOpenClose :: (MonadError String m) => GpioT m ()
 testOpenClose =
   do descriptor <- open (Pin 1)
      case descriptor of
        Left e -> throwError e
-       Right d ->
-         do close d
-            return "OK"
+       Right d -> close d
 
 spec :: Spec
 spec =
   do describe "open and close" $
 
        do it "succeeds when the pin is available" $
-            do let expectedResult = (Right "OK", emptyWorld, ["Open Pin 1", "Close PinDescriptor (Pin 1)"])
+            do let expectedResult = (Right (), emptyWorld, ["Open Pin 1", "Close PinDescriptor (Pin 1)"])
                runMock (Env $ Set.fromList [Pin 1]) testOpenClose `shouldBe` expectedResult
 
           it "fails when the pin is unavailable" $
