6ed3feac4fa65ad23ac3a21dff8333e82f73a775
Drew Hess <src@drewhess.com>
2015-11-30 15:34:58 -0800

    Add tests for Mock getDirection/setDirection.

--- test/System/GPIOSpec.hs
+++ test/System/GPIOSpec.hs
@@ -18,6 +18,20 @@ testOpenClose =
        Left e -> throwError e
        Right d -> close d
 
+testGetSetDirection :: (MonadError String m) => (GpioT String) m Direction
+testGetSetDirection =
+  do descriptor <- open (Pin 1)
+     case descriptor of
+       Left e -> throwError e
+       Right d ->
+         do dir1 <- getDirection d
+            case dir1 of
+              In -> setDirection d Out
+              Out -> setDirection d In
+            dir2 <- getDirection d
+            close d
+            return dir2
+
 spec :: Spec
 spec =
   do describe "open and close" $
@@ -29,3 +43,9 @@ spec =
           it "fails when the pin is unavailable" $
             do let expectedResult = (Left "Open failed: Pin 1 does not exist", Map.empty, [])
                runMock (Set.fromList [Pin 2]) testOpenClose `shouldBe` expectedResult
+
+     describe "getDirection and setDirection" $
+
+       do it "gets and sets pin input/output direction" $
+            let expectedResult = (Right Out, Map.empty, ["Open Pin 1", "Set direction: PinDescriptor (Pin 1) Out", "Close PinDescriptor (Pin 1)"])
+            in runMock (Set.fromList [Pin 1]) testGetSetDirection `shouldBe` expectedResult
