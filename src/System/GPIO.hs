79884e719959378589aa847bf8c7574d6ed17620
Drew Hess <src@drewhess.com>
2015-12-11 15:43:42 -0800

    Add top-level documentation.
    Clean up top-level exports a bit, as well.

--- src/System/GPIO.hs
+++ src/System/GPIO.hs
@@ -1,7 +1,20 @@
+-- | An embedded DSL ("eDSL") for writing platform-independent GPIO
+-- programs in Haskell.
+--
+-- Currently only the Linux "sysfs" GPIO system is supported, but
+-- other platforms will be supported in the future.
+--
+-- Additionally, the package provides a "mock" GPIO environment for
+-- testing programs on platforms that don't provide real GPIO
+-- capabilities. That module is not exported from here, but you can
+-- access it by directly importing "System.GPIO.Mock".
+
 module System.GPIO
-       ( module System.GPIO.Free
+       ( -- * The abstract GPIO eDSL
+         module System.GPIO.Free
+         -- * The Linux sysfs GPIO interpreter
        , module System.GPIO.Linux.Sysfs
        ) where
 
 import System.GPIO.Free
-import System.GPIO.Linux.Sysfs
+import System.GPIO.Linux.Sysfs (SysfsF, SysfsT, Sysfs, runSysfs, runSysfs', runSysfsSafe)
