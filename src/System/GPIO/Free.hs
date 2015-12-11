d3010d3832732d809574119392b1417ca35d1e0d
Drew Hess <src@drewhess.com>
2015-12-11 13:44:10 -0800

    Documentation for the eDSL.

--- src/System/GPIO/Free.hs
+++ src/System/GPIO/Free.hs
@@ -1,3 +1,21 @@
+-- | An eDSL for GPIO computations.
+--
+-- To make it easier to use the eDSL in typical scenarios, the eDSL is
+-- implemented as a transformer using the 'FreeT' transformer.
+--
+-- As the acronym implies, GPIO means performing I/O; therefore
+-- unexpected errors may occur during program excecution. The 'GpioF'
+-- eDSL makes allowances for "expected" failures (e.g., attempting to
+-- open a pin may fail). How unxpected failures are handled is a
+-- detail left to each particular interpreter. The interpreters
+-- included in the 'gpio' package strive to cleanly separate errors
+-- which occur in the program itself (semantic errors, e.g., writing
+-- to a pin that is configured for input), errors which occur in the
+-- interpreter implementation (e.g., reading an unexpected value from
+-- a sytem-level GPIO interface), and errors which occur during I/O
+-- (e.g., while still in use by the program, a pin is unexpectedly
+-- unexported by the system or by another program.)
+
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GADTs #-}
@@ -6,7 +24,6 @@
 
 module System.GPIO.Free
        ( GpioF(..)
-       , GpioM
        , GpioT
        , Pin(..)
        , PinDirection(..)
@@ -23,15 +40,18 @@ module System.GPIO.Free
 
 import Control.Monad.Trans.Free (FreeT, MonadFree, liftF)
 import Control.Monad.Free.TH (makeFreeCon)
-import Data.Functor.Identity (Identity)
 import GHC.Generics
 
+-- | A GPIO pin, identified by pin number.
 data Pin = Pin Int deriving (Eq, Ord, Show, Generic)
 
+-- | Pin direction (input/output).
 data PinDirection = In | Out deriving (Eq, Show, Generic)
 
+-- | Pin value (high/low voltage).
 data Value = Low | High deriving (Eq, Enum, Ord, Show, Generic)
 
+-- | Commands for the GPIO eDSL.
 data GpioF e h m next where
   Pins :: ([Pin] -> next) -> GpioF e h m next
   Open :: Pin -> (Either e h -> next) -> GpioF e h m next
@@ -52,15 +72,71 @@ instance Functor (GpioF e h m) where
   fmap f (WritePin h v x) = WritePin h v (f x)
   fmap f (WithPin p block g) = WithPin p block (f . g)
 
+-- | A transformer which adds GPIO programs to a monad stack. The 'e'
+-- type parameter is an exception type for representing errors in the
+-- program; 'h' is an abstract pin handle for operating on opened
+-- pins; and 'm' is the wrapped monad.
+--
+-- Note that the 'm' parameter is required in order to implement the
+-- 'WithPin' command, which runs programs within programs and is
+-- therefore dependent on the wrapped monad type.
 type GpioT e h m = FreeT (GpioF e h m)
 
-type GpioM h = (GpioT String h Identity) Identity
-
+-- | Get a list of available GPIO pins on the system.
+--
+-- This command makes a best-effort attempt to find the available
+-- pins, but some systems may not make the complete list available at
+-- runtime. Therefore, there may be more pins available than are
+-- returned by this function.
 makeFreeCon 'Pins
+
+-- | Open a pin for reading and writing. If the pin can be opened,
+-- the function returns a pin handle, which is used to operate on the
+-- pin. If the pin cannot be opened (e.g., due to a permissions
+-- failure), the function returns an error value.
 makeFreeCon 'Open
+
+-- | Close a pin; i.e., indicate to the system that you no longer
+-- intend to use the pin via the given handle.
 makeFreeCon 'Close
+
+-- | Get the pin's 'PinDirection'.
+--
+-- Note that it is not uncommon for a pin's direction to be immutable,
+-- i.e., to be hard-wired as 'In' or 'Out'. On some systems (e.g.,
+-- "System.GPIO.Linux.Sysfs"), the direction of hard-wired pins is not
+-- made available at run-time. In such cases, this function returns
+-- 'Nothing'.
 makeFreeCon 'Direction
+
+-- | Set the pin's 'PinDirection'.
+--
+-- As some pins' direction cannot be changed, you should first call
+-- 'direction' to make sure this particular pin's direction is
+-- configurable.
 makeFreeCon 'SetDirection
+
+-- | Read the pin's 'Value'.
 makeFreeCon 'ReadPin
+
+-- | Set the pin's 'Value'. It is an error to call this function when
+-- the pin is not configured for output.
 makeFreeCon 'WritePin
+
+-- | Exception-safe pin management.
+--
+-- 'withPin' opens a pin using 'open' and passes the handle to the
+-- given GPIO computation. Upon completion of the computation or an
+-- exception occuring within the computation, 'withPin' closes the
+-- handle using 'close' and then propagates the result, either by
+-- returning the value of the computation or by re-raising the
+-- exception. If closing the handle raises an exception, this
+-- exception will be raised by 'withPin'.
+--
+-- Note: because the eDSL is abstract, it can make no guarantees about
+-- the implementation of 'withPin' as that is left to each particular
+-- interpreter. However, interpreters should strive to ensure that the
+-- pin is closed properly no matter what kind of exception occurs,
+-- regardless of how that exception is then expressed to the caller by
+-- the implementation.
 makeFreeCon 'WithPin
