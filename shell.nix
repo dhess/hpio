eead3bc32e298c8771d1894dd5458fd65ff23078
Drew Hess <src@drewhess.com>
2015-12-12 16:58:40 -0800

    Release v0.3.0.

--- shell.nix
+++ shell.nix
@@ -10,7 +10,7 @@ let
       }:
       mkDerivation {
         pname = "gpio";
-        version = "0.2.0";
+        version = "0.3.0";
         src = ./.;
         libraryHaskellDepends = [
           base containers directory errors exceptions filepath free mtl
