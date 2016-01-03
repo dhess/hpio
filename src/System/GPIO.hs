fa8153aad8adb437edebe04b02c4cfd0207627f9
Drew Hess <src@drewhess.com>
2016-01-03 18:06:15 +0000

    Remove the old 'Mock' GPIO interpreter.

--- src/System/GPIO.hs
+++ src/System/GPIO.hs
@@ -1,15 +1,19 @@
 -- | Top-level exports for the GPIO eDSL.
 --
--- Because it's a relatively special case, the mock I/O testing monad
--- is not exported from here, but you can access it by directly
--- importing "System.GPIO.Mock".
+-- Because it's generally only used for testing, the
+-- 'System.GPIO.Linux.SysfsMock.SysfsMockT' monad transformer is not
+-- exported from here, but you can access it by directly importing
+-- "System.GPIO.Linux.SysfsMock".
 
 module System.GPIO
        ( -- * The abstract GPIO eDSL
          module System.GPIO.Free
          -- * The Linux sysfs GPIO interpreter
        , module System.GPIO.Linux.Sysfs
+         -- * The Linux sysfs GPIO monad.
+       , module System.GPIO.Linux.SysfsIO
        ) where
 
 import System.GPIO.Free
-import System.GPIO.Linux.Sysfs (SysfsF, SysfsT, runSysfsT)
+import System.GPIO.Linux.Sysfs (MonadSysfs(..), SysfsF, SysfsT, runSysfsT)
+import System.GPIO.Linux.SysfsIO (SysfsIOT(..), SysfsIO, runSysfsIO, runSysfsIO', runSysfsIOSafe)
