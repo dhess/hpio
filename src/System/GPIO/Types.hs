5253ee5eaf6477e5615ebe7fbe08f772dd003103
Drew Hess <src@drewhess.com>
2016-01-07 18:15:33 -0800

    Beef up the GPIO basic types.
    More useful 'deriving' clauses, and make 'PinValue' an instance of
    'Bits' and 'FiniteBits' so it can be treated like a logical 0/1 value.

--- src/System/GPIO/Types.hs
+++ src/System/GPIO/Types.hs
@@ -1,6 +1,8 @@
 -- | Basic GPIO types.
 
+{-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 module System.GPIO.Types
        ( -- * GPIO pins
@@ -8,6 +10,7 @@ module System.GPIO.Types
        , PinDirection(..)
        , PinValue(..)
          -- * Convenience functions
+       , pinNumber
        , invertDirection
        , invertValue
          -- * PinValue <-> Bool conversions
@@ -15,16 +18,87 @@ module System.GPIO.Types
        , boolToValue
        ) where
 
+import Data.Bits
+import Data.Data
+import Data.Ix
 import GHC.Generics
+import Test.QuickCheck
 
 -- | A GPIO pin, identified by pin number.
-data Pin = Pin Int deriving (Eq, Ord, Show, Generic)
+newtype Pin =
+  Pin Int
+  deriving (Bounded,Enum,Eq,Data,Ord,Read,Ix,Show,Generic)
+
+instance Arbitrary Pin where
+  arbitrary = arbitraryBoundedEnum
+  shrink = genericShrink
+
+-- | Get the pin number as an 'Int'.
+pinNumber :: Pin -> Int
+pinNumber (Pin n) = n
 
 -- | Pin direction (input/output).
-data PinDirection = In | Out deriving (Eq, Show, Generic)
+data PinDirection
+  = In
+  | Out
+  deriving (Bounded,Enum,Eq,Data,Ord,Read,Show,Ix,Generic)
+
+instance Arbitrary PinDirection where
+  arbitrary = arbitraryBoundedEnum
+  shrink = genericShrink
 
 -- | Pin value (high/low voltage).
-data PinValue = Low | High deriving (Eq, Enum, Ord, Show, Generic)
+data PinValue
+  = Low
+  | High
+  deriving (Bounded,Enum,Eq,Data,Ord,Read,Show,Ix,Generic)
+
+instance Bits PinValue where
+  High .&. High = High
+  _    .&. _    = Low
+
+  Low  .|. Low  = Low
+  _    .|. _    = High
+
+  Low  `xor` Low  = Low
+  Low  `xor` High = High
+  High `xor` Low  = High
+  High `xor` High = Low
+
+  complement Low = High
+  complement High = Low
+
+  shift x 0 = x
+  shift _ _ = Low
+
+  rotate x _ = x
+
+  bit 0 = High
+  bit _ = Low
+
+  testBit x 0 = valueToBool x
+  testBit _ _ = False
+
+  bitSizeMaybe _ = Just 1
+
+  bitSize _ = 1
+
+  isSigned _ = False
+
+  popCount Low  = 0
+  popCount High = 1
+
+instance FiniteBits PinValue where
+  finiteBitSize _ = 1
+
+  countTrailingZeros Low  = 1
+  countTrailingZeros High = 0
+
+  countLeadingZeros = countTrailingZeros
+
+instance Arbitrary PinValue where
+  arbitrary = arbitraryBoundedEnum
+  shrink = genericShrink
 
 -- | Invert a 'PinDirection' value.
 invertDirection :: PinDirection -> PinDirection
@@ -33,8 +107,7 @@ invertDirection Out = In
 
 -- | Invert a 'PinValue'.
 invertValue :: PinValue -> PinValue
-invertValue High = Low
-invertValue Low = High
+invertValue = complement
 
 valueToBool :: PinValue -> Bool
 valueToBool Low  = False
@@ -43,3 +116,4 @@ valueToBool High = True
 boolToValue :: Bool -> PinValue
 boolToValue False = Low
 boolToValue True  = High
+
---
+++ test/System/GPIO/TypesSpec.hs
@@ -0,0 +1,126 @@
+module System.GPIO.TypesSpec (spec) where
+
+import System.GPIO.Types
+
+import Data.Bits
+import Test.Hspec
+import Test.QuickCheck (property)
+
+-- Test all our hand-derived instances and functions.
+
+-- PinValue mimics Bool with respect to Bits and FiniteBits.
+--
+ib2v :: (Int -> Bool) -> Int -> PinValue
+ib2v f n = boolToValue $ f n
+
+bi2v :: (Bool -> Int) -> PinValue -> Int
+bi2v f a = f (valueToBool a)
+
+bb2v :: (Bool -> Bool) -> PinValue -> PinValue
+bb2v f a = boolToValue $ f (valueToBool a)
+
+bbb2v :: (Bool -> Bool -> Bool) -> PinValue -> PinValue -> PinValue
+bbb2v f a b = boolToValue $ f (valueToBool a) (valueToBool b)
+
+bib2v :: (Bool -> Int -> Bool) -> PinValue -> Int -> PinValue
+bib2v f a n = boolToValue $ f (valueToBool a) n
+
+spec :: Spec
+spec =
+  do describe "Pin" $
+       do it "pinNumber" $ property $
+            \p@(Pin n) -> n == pinNumber p
+
+     describe "PinDirection" $
+       do it "invertDirection" $
+            invertDirection In == Out
+              && invertDirection Out == In
+
+     describe "PinValue" $
+       do it "invertValue" $ property $
+            \a -> invertValue a == complement a
+
+          it "valueToBool" $
+            valueToBool Low == False
+              && valueToBool High == True
+
+          it "boolToValue" $
+            boolToValue False == Low
+              && boolToValue True == High
+
+          context "implements the Bits typeclass" $
+           do it "(.&.)" $ property $
+                \a b -> a .&. b == bbb2v (.&.) a b
+
+              it "(.|.)" $ property $
+                \a b -> a .|. b == bbb2v (.|.) a b
+
+              it "xor" $ property $
+                \a b -> a `xor` b == bbb2v xor a b
+
+              it "complement" $ property $
+                \a -> complement a == bb2v complement a
+
+              it "shift" $ property $
+                \a n -> a `shift` n == bib2v shift a n
+
+              it "rotate" $ property $
+                \a n -> a `rotate` n == bib2v rotate a n
+
+              it "zeroBits" $ property $
+                (zeroBits :: PinValue) == boolToValue (zeroBits:: Bool)
+
+              it "bit" $ property $
+                \n -> bit n == ib2v bit n
+
+              it "setBit" $ property $
+                \a n -> a `setBit` n == bib2v setBit a n
+
+              it "clearBit" $ property $
+                \a n -> a `clearBit` n == bib2v clearBit a n
+
+              it "complementBit" $ property $
+                \a n -> a `complementBit` n == bib2v complementBit a n
+
+              it "testBit" $ property $
+                \a n -> testBit a n == testBit (valueToBool a) n
+
+              it "bitSizeMaybe" $ property $
+                \a -> bitSizeMaybe a == bitSizeMaybe (valueToBool a)
+
+              it "bitSize" $ property $
+                \a -> bitSize a == bitSize (valueToBool a)
+
+              it "isSigned" $ property $
+                \a -> isSigned a == isSigned (valueToBool a)
+
+              it "shiftL" $ property $
+                \a n -> a `shiftL` n == bib2v shiftL a n
+
+              it "unsafeShiftL" $ property $
+                \a n -> a `unsafeShiftL` n == bib2v unsafeShiftL a n
+
+              it "shiftR" $ property $
+                \a n -> a `shiftR` n == bib2v shiftR a n
+
+              it "unsafeShiftR" $ property $
+                \a n -> a `unsafeShiftR` n == bib2v unsafeShiftR a n
+
+              it "rotateL" $ property $
+                \a n -> a `rotateL` n == bib2v rotateL a n
+
+              it "rotateR" $ property $
+                \a n -> a `rotateR` n == bib2v rotateR a n
+
+              it "popCount" $ property $
+                \a -> popCount a == popCount (valueToBool a)
+
+          context "implements the FiniteBits typeclass" $
+            do it "finiteBitSize" $ property $
+                 \a -> finiteBitSize a == bi2v finiteBitSize a
+
+               it "countLeadingZeros" $ property $
+                 \a -> countLeadingZeros a == bi2v countLeadingZeros a
+
+               it "countTrailingZeros" $ property $
+                 \a -> countTrailingZeros a == bi2v countTrailingZeros a
