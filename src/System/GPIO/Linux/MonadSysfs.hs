e0f7fa0d3ad109224801b47469dd072455a30c50
Drew Hess <src@drewhess.com>
2016-01-28 18:54:52 -0800

    Document 'MonadSysfs' so that it can be used directly.
    Not encouraged, but it does avoid the 'Free' monad overhead, and that
    might be important in some cases.

--- src/System/GPIO/Linux/MonadSysfs.hs
+++ src/System/GPIO/Linux/MonadSysfs.hs
@@ -1,5 +1,24 @@
 -- | Monads in which low-level Linux 'sysfs' GPIO operations can be
 -- embedded.
+--
+-- The chief purpose of this typeclass is to abstract the low-level
+-- mechanisms from the 'sysfs' GPIO interface, so that a "mock"
+-- implementation can be used for testing 'System.GPIO.Free.GpioF'
+-- programs as if they were running on an actual Linux system with a
+-- working 'sysfs'. Generally speaking, you should not use this
+-- typeclass to write GPIO programs, and should instead use the
+-- 'System.GPIO.Linux.Sysfs.runSysfsT' interpreter on
+-- 'System.GPIO.Free.GpioF' programs so that your GPIO programs can
+-- work portably across multiple GPIO implementations (and also
+-- because the 'System.GPIO.Free.GpioF' has niceties like resource
+-- bracketing for exception safety).
+--
+-- However, if you really want to write low-level, Linux-specific GPIO
+-- programs without the overhead of the
+-- 'Control.Monad.Trans.Free.FreeT' transformer, you can use this
+-- typeclass directly. Do keep in mind, however, that this interface
+-- may change at any time in order to suit the needs of the
+-- 'System.GPIO.Free.GpioF' interpreter!
 
 module System.GPIO.Linux.MonadSysfs
        ( -- * MonadSysfs class
@@ -80,28 +99,124 @@ pinValueFileName p = pinDirName p </> "value"
 -- | Monads in which low-level Linux 'sysfs' GPIO-like operations may be
 -- embedded.
 --
--- The purpose of this typeclass is to abstract the low-level
--- mechanisms from the interface, chiefly so that a "mock"
--- implementation can be used for testing 'System.GPIO.Free.GpioF'
--- programs as if they were running on an actual Linux system with a
--- working 'sysfs'.
+-- Note that, in the spirit of Postel's law, functions which read
+-- values from the 'sysfs' GPIO filesystem return a 'String' with the
+-- raw 'sysfs' value returned by the kernel; but functions which write
+-- values to 'sysfs' GPIO files take properly-typed values
+-- corresponding to their 'System.GPIO.Free.GpioF' type. (This is
+-- primarily done so that the conversion to/from 'String's need only
+-- be done once in the 'System.GPIO.Linux.Sysfs.runSysfsT'
+-- interpreter, instead of being needlessly repeated in each
+-- 'MonadSysfs' implementation.)
 class (Monad m) => MonadSysfs m where
+
+  -- | Test whether the 'sysfs' GPIO filesystem is available.
   sysfsIsPresent :: m Bool
+
+  -- | Test whether the given pin is already exported.
   pinIsExported :: Pin -> m Bool
+
+  -- | Test whether the given pin's direction can be set.
   pinHasDirection :: Pin -> m Bool
+
+  -- | Export the given pin.
   exportPin :: Pin -> m ()
+
+  -- | Unexport the given pin.
+  --
+  -- It is an error to call this function if the pin is not currently
+  -- exported.
   unexportPin :: Pin -> m ()
+
+  -- | Read the given pin's direction as a 'String'. The returned
+  -- value should be either "in\\n" or "out\\n" (note the trailing
+  -- newline); any other value should be treated as an error.
+  --
+  -- It is an error to call this function if the pin has no
+  -- user-visible direction in the 'sysfs' GPIO filesystem. (Some
+  -- platforms may make the pin's direction available via other
+  -- mechanisms, however.)
   readPinDirection :: Pin -> m String
+
+  -- | Set the given pin's direction. Note that the function will
+  -- convert the given 'PinDirection' value to the corresponding
+  -- string value expected by the 'sysfs' GPIO filesystem.
+  --
+  -- It is an error to call this function if the pin has no
+  -- user-visible direction in the 'sysfs' GPIO filesystem. (Some
+  -- platforms may make the pin's direction settable via other
+  -- mechanisms, however.)
   writePinDirection :: Pin -> PinDirection -> m ()
+
+  -- | Pins whose direction can be set can be configured for output by
+  -- writing a 'PinValue' to their sysfs "direction" attribute. This
+  -- enables glitch-free output configuration, assuming the pin is
+  -- currently configured for input, or some kind of tri-stated or
+  -- floating high-impedance mode.
+  --
+  -- It is an error to call this function if the pin has no
+  -- user-visible direction in the 'sysfs' GPIO filesystem. (Some
+  -- platforms may make the pin's direction settable via other
+  -- mechanisms, however.)
   writePinDirectionWithValue :: Pin -> PinValue -> m ()
+
+  -- | Read the given pin's value as a 'String'. The returned value
+  -- should be either "0\\n" or "1\\n" (note the trailing newline); any
+  -- other value should be treated as an error.
+  --
+  -- Note that this function never blocks, regardless of the pin's
+  -- 'PinReadTrigger' setting.
   readPinValue :: Pin -> m String
+
+  -- | A blocking version of 'readPinValue'. The current thread will
+  -- block until an event occurs on the pin as specified by the pin's
+  -- current 'PinReadTrigger' setting.
+  --
+  -- If your program is compiled with GHC using the @-threaded@
+  -- option, this function will not block other Haskell threads.
   threadWaitReadPinValue :: Pin -> m String
+
+  -- | Set the given pin's value. Note that the function will convert
+  -- the given 'PinValue' value to the corresponding string value
+  -- expected by the 'sysfs' GPIO filesystem.
+  --
+  -- It is an error to call this function if the pin is configured as
+  -- an input pin.
   writePinValue :: Pin -> PinValue -> m ()
+
+  -- | Test whether the pin has an "edge" 'sysfs' attribute, i.e.,
+  -- whether it can be configured for edge- or level-triggered
+  -- interrupts.
   pinHasEdge :: Pin -> m Bool
+
+  -- | Read the given pin's "edge" 'sysfs' attribute as a 'String'.
+  -- The returned value should be one of "none\\n", "rising\\n",
+  -- "falling\\n", or "both\\n" (note the trailing newline). Any other
+  -- values should be treated as an error.
+  --
+  -- It is an error to call this function when the pin has no "edge"
+  -- attribute.
   readPinEdge :: Pin -> m String
+
+  -- | Write the given pin's "edge" 'sysfs' attribute.
+  --
+  -- It is an error to call this function when the pin has no "edge"
+  -- attribute.
   writePinEdge :: Pin -> PinReadTrigger -> m ()
+
+  -- | Read the given pin's "active_low" 'sysfs' attribute as a
+  -- 'String'. The returned value should be either "0\\n" or "1\\n"
+  -- (note the trailing newline). Any other values should be treated
+  -- as an error.
   readPinActiveLow :: Pin -> m String
+
+  -- | Write the given pin's "active_low" 'sysfs' attribute.
   writePinActiveLow :: Pin -> Bool -> m ()
+
+  -- | Return a list of all pins that are exposed via the 'sysfs' GPIO
+  -- filesystem. Note that the returned list may omit some pins that
+  -- are available on the host but which, for various reasons, are not
+  -- exposed via the 'sysfs' GPIO interface.
   availablePins :: m [Pin]
 
 instance (MonadIO m, MonadSysfs m) => MonadSysfs (ContT r m) where
