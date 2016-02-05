ef326c1041025bf066d1a75a6ed0c8942c222143
Drew Hess <src@drewhess.com>
2016-02-04 16:56:33 -0800

    Add a 'listPins' command to EdgeRead.hs.

--- examples/EdgeRead.hs
+++ examples/EdgeRead.hs
@@ -1,11 +1,13 @@
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
 import Control.Concurrent (forkIO, threadDelay)
 import Control.Monad (forever, void)
 import Control.Monad.IO.Class (MonadIO, liftIO)
+import Data.Foldable (for_)
 import Options.Applicative
 import System.GPIO.Free
 import System.GPIO.Linux.Sysfs
@@ -13,58 +15,85 @@ import System.GPIO.Linux.SysfsIO (runSysfsIO)
 import System.GPIO.Types
 import System.Timeout (timeout)
 
+-- Only one for now.
+data Interpreter =
+  SysfsIO
+  deriving (Eq,Show,Read)
+
 data GlobalOptions =
-  GlobalOptions {_delay :: Int
-                ,_trigger :: PinReadTrigger
-                ,_timeout :: Int
+  GlobalOptions {_interpreter :: Interpreter
                 ,_cmd :: Command}
 
 data Command
-  = Sysfs SysfsOptions
+  = ListPins
+  | ReadTrigger ReadTriggerOptions
 
-data SysfsOptions =
-  SysfsOptions {_outputPin :: Pin
-               ,_inputPin :: Pin}
+listPinsCmd :: Parser Command
+listPinsCmd = pure ListPins
 
-sysfsCmd :: Parser Command
-sysfsCmd = Sysfs <$> sysfsOptions
+data ReadTriggerOptions =
+  ReadTriggerOptions {_period :: Int
+                     ,_trigger :: PinReadTrigger
+                     ,_timeout :: Int
+                     ,_outputPin :: Pin
+                     ,_inputPin :: Pin}
 
-sysfsOptions :: Parser SysfsOptions
-sysfsOptions =
-  SysfsOptions <$>
-    argument auto (metavar "INPIN") <*>
+readTriggerCmd :: Parser Command
+readTriggerCmd = ReadTrigger <$> readTriggerOptions
+
+oneSecond :: Int
+oneSecond = 1 * 1000000
+
+readTriggerOptions :: Parser ReadTriggerOptions
+readTriggerOptions =
+  ReadTriggerOptions <$>
+    option auto (long "period" <>
+                 short 'p' <>
+                 metavar "INT" <>
+                 value oneSecond <>
+                 showDefault <>
+                 help "Delay between output toggles (in ms)") <*>
+    option auto (long "trigger" <>
+                 short 't' <>
+                 metavar "TRIGGER" <>
+                 value Level <>
+                 showDefault <>
+                 help "Event on which to trigger (None, RisingEdge, FallingEdge, or Level)") <*>
+    option auto (long "timeout" <>
+                 short 'T' <>
+                 metavar "TIMEOUT" <>
+                 value (-1) <>
+                 help "Use a timeout for readPin (in ms)") <*>
+    argument auto (metavar "INPIN")  <*>
     argument auto (metavar "OUTPIN")
 
 cmds :: Parser GlobalOptions
 cmds =
   GlobalOptions <$>
-  option auto (long "delay" <>
-               short 'd' <>
-               metavar "DELAY" <>
-               value 1 <>
-               showDefault <>
-               help "Delay between output toggles (in seconds)") <*>
-  option auto (long "trigger" <>
-               short 't' <>
-               metavar "TRIGGER" <>
-               value Level <>
-               showDefault <>
-               help "Event on which to trigger (None, RisingEdge, FallingEdge, or Level)") <*>
-  option auto (long "timeout" <>
-               short 'T' <>
-               metavar "TIMEOUT" <>
-               value (-1) <>
-               help "Use a timeout for readPin (in ms)") <*>
-  hsubparser
-    (command "sysfs" (info sysfsCmd (progDesc "Use the Linux sysfs interpreter to drive INPIN using OUTPIN. (Make sure the pins are connected!")))
+    option auto (long "interpreter" <>
+                 short 'i' <>
+                 metavar "SysfsIO" <>
+                 value SysfsIO <>
+                 showDefault <>
+                 help "Choose the GPIO interpreter (system) to use") <*>
+    hsubparser
+      (command "listPins" (info listPinsCmd (progDesc "List the GPIO pins available on the system")) <>
+       command "readTrigger" (info readTriggerCmd (progDesc "Use the Linux sysfs interpreter to drive INPIN using OUTPIN. (Make sure the pins are connected!")))
 
 run :: GlobalOptions -> IO ()
-run (GlobalOptions delay trigger to (Sysfs (SysfsOptions inputPin outputPin))) =
+run (GlobalOptions SysfsIO (ReadTrigger (ReadTriggerOptions period trigger to inputPin outputPin))) =
   do void $ forkIO (void $ runSysfsIO $ edgeRead inputPin trigger to)
-     runSysfsIO $ driveOutput outputPin delay
+     runSysfsIO $ driveOutput outputPin period
+run (GlobalOptions SysfsIO ListPins) = runSysfsIO listPins
 
 output :: (MonadIO m) => String -> m ()
 output = liftIO . putStrLn
+
+listPins :: (MonadIO m) => GpioT h m m ()
+listPins =
+  pins >>= \case
+    [] -> output "No GPIO pins found on this system"
+    ps -> for_ ps $ liftIO . print
 
 -- This must be the 'SysfsT' monad rather than the more generic
 -- 'GpioT' because we need to 'runSysfsIO' inside it, in order to use
