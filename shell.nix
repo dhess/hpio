17ddf2076705adcf074e90b08d2cde8aab1bdaf5
Drew Hess <src@drewhess.com>
2017-01-11 14:59:51 -1000

    Bump version for upcoming release.

--- shell.nix
+++ shell.nix
@@ -11,7 +11,7 @@ let
       }:
       mkDerivation {
         pname = "hpio";
-        version = "0.8.0.4";
+        version = "0.8.0.5";
         src = ./.;
         isLibrary = true;
         isExecutable = true;
