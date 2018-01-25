f8be10351e6aa8582525cdf58c173f1a1e314347
Drew Hess <src@drewhess.com>
2018-01-25 00:30:35 -0800

    A self-contained shell.nix.

--- shell.nix
+++ shell.nix
@@ -1,3 +1,15 @@
-{ compiler ? "ghc822" }:
+{ compiler ? "ghc822"
+, overlays ? [ (import ./.) ]
+}:
 
-(import ./nix/release.nix { inherit compiler; }).hpio.env
+let
+
+  fixedNixPkgs = (import ./nix/lib.nix).fetchNixPkgs;
+
+  pkgs = (import fixedNixPkgs) { inherit overlays; };
+
+  drv = pkgs.haskellPackages.hpio;
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
