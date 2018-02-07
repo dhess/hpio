50cc03db25ed79e0e6f0f848229ebf4ad32d95a7
Drew Hess <dhess-src@quixoftic.com>
2018-02-07 00:22:27 -0800

    Make "next" failed Hydra jobs more visible in the Hydra interface.

--- nix/jobsets/next.nix
+++ nix/jobsets/next.nix
@@ -22,6 +22,25 @@ with import (fixedNixPkgs + "/pkgs/top-level/release-lib.nix") {
 
 let
 
+  ## Aggregates are handy for defining jobs (especially for subsets of
+  ## platforms), but they don't provide very useful information in
+  ## Hydra, especially when they die. We use aggregates here to define
+  ## set of jobs, and then splat them into the output attrset so that
+  ## they're more visible in Hydra.
+
+  enumerateConstituents = aggregate: lib.listToAttrs (
+    map (d:
+           let
+             name = (builtins.parseDrvName d.name).name;
+             system = d.system;
+           in
+             { name = name + "." + system;
+               value = d;
+             }
+         )
+        aggregate.constituents
+  );
+
   jobs = {
 
     ghc841 = pkgs.releaseTools.aggregate {
@@ -43,3 +62,4 @@ in
 {
   inherit (jobs) ghc841;
 }
+// enumerateConstituents jobs.ghc841
