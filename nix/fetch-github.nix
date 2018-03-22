7c2da14f47c342634402dac41db4913127ab8e7a
Drew Hess <dhess-src@quixoftic.com>
2018-03-21 18:41:42 -0700

    Switch to the Nix 2.0 way of pinning packages.

--- nix/fetch-github.nix
+++ nix/fetch-github.nix
@@ -1,28 +1,11 @@
-# Adapted from
-# https://github.com/input-output-hk/iohk-ops/blob/a25d2d9320f8b8f3515f1195d5fceba96fd24493/fetch-nixpkgs.nix
-
 { jsonSpec }:
 
 let
+
   spec = builtins.fromJSON jsonSpec;
-  src = import <nix/fetchurl.nix> {
+  src = builtins.fetchTarball {
     url = "https://github.com/${spec.owner}/${spec.repo}/archive/${spec.rev}.tar.gz";
     inherit (spec) sha256;
   };
-  nixcfg = import <nix/config.nix>;
-in builtins.derivation {
-  system = builtins.currentSystem;
-  name = "${src.name}-unpacked";
-  builder = builtins.storePath nixcfg.shell;
-  inherit src;
-  args = [
-    (builtins.toFile "builder" ''
-      $coreutils/mkdir $out
-      cd $out
-      $gzip -d < $src | $tar -x --strip-components=1
-    '')
-  ];
-  coreutils = builtins.storePath nixcfg.coreutils;
-  tar = builtins.storePath nixcfg.tar;
-  gzip = builtins.storePath nixcfg.gzip;
-}
+
+in src
