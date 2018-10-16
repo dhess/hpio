24144eab9f235a881bccc45438fde6df40932186
Drew Hess <dhess-src@quixoftic.com>
2018-10-16 15:09:13 -0400

    "next" jobset: add ghc844, add aarch64-linux to supportedSystems.

--- nix/jobsets/next.nix
+++ nix/jobsets/next.nix
@@ -8,7 +8,7 @@ let
 
 in
 
-{ supportedSystems ? [ "x86_64-darwin" "x86_64-linux" ]
+{ supportedSystems ? [ "x86_64-darwin" "x86_64-linux" "aarch64-linux" ]
 , scrubJobs ? true
 , nixpkgsArgs ? {
     config = { allowUnfree = true; allowBroken = true; inHydra = true; };
@@ -24,23 +24,36 @@ let
 
   jobs = {
 
+    ghc844 = pkgs.releaseTools.aggregate {
+      name = "ghc844";
+      meta.description = "hpio built against nixpkgs haskellPackages using GHC 8.4.4";
+      constituents = with jobs; [
+        haskellPackages844.hpio.x86_64-darwin
+        haskellPackages844.hpio.x86_64-linux
+        haskellPackages844.hpio.aarch64-linux
+      ];
+    };
+
     ghc861 = pkgs.releaseTools.aggregate {
       name = "ghc861";
       meta.description = "hpio built against nixpkgs haskellPackages using GHC 8.6.1";
       constituents = with jobs; [
         haskellPackages861.hpio.x86_64-darwin
         haskellPackages861.hpio.x86_64-linux
+        haskellPackages861.hpio.aarch64-linux
       ];
     };
 
   } // (mapTestOn ({
 
     haskellPackages861 = packagePlatforms pkgs.haskellPackages861;
+    haskellPackages844 = packagePlatforms pkgs.haskellPackages844;
 
   }));
 
 in
 {
-  inherit (jobs) ghc861;
+  inherit (jobs) ghc861 ghc844;
 }
 // pkgs.lib.testing.enumerateConstituents jobs.ghc861
+// pkgs.lib.testing.enumerateConstituents jobs.ghc844
--- nix/overlays/haskell-overrides-next.nix
+++ nix/overlays/haskell-overrides-next.nix
@@ -9,6 +9,13 @@ in
 
   ## Testing with upcoming GHC releases.
 
+  haskellPackages8844 =
+    withOurHpioHlint (self.haskell.packages.ghc844.extend (self: super:
+      with haskell.lib;
+      rec {
+      }
+    ));
+
   haskellPackages861 =
     withOurHpioHlint (self.haskell.packages.ghc861.extend (self: super:
       with haskell.lib;
