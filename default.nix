80c2f6b6be06652d6ca8d7b2250c74e71b924c62
Drew Hess <src@drewhess.com>
2019-02-19 21:35:16 +0000

    Provide access to the local hpio.nix derivation.

--- default.nix
+++ default.nix
@@ -11,10 +11,16 @@ let
 
   inherit (localLib.dhess-lib-nix) lib;
   localOverlays = import ./nix/overlays.nix;
+  hpioNix = nix/pkgs/hpio.nix;
   self = lib.customisation.composeOverlays (lib.singleton localOverlays) pkgs;
 
 in
 {
+  # haskellPackages with the local hpio package.
   inherit (self) haskellPackages;
+
+  # The path to the local hpio.nix, in case you want to make your own.
+  inherit hpioNix;
+
   overlays.all = localOverlays;
 }
