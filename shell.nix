515c1d3e3a41e4b327aca5dea1153dc37a230628
Drew Hess <src@drewhess.com>
2019-02-19 08:59:05 +0000

    Replace shell.nix with a much simpler expression.

--- shell.nix
+++ shell.nix
@@ -1,14 +1,1 @@
-let
-
-  localLib = import nix/lib.nix;
-
-  overlays = [
-    (import nix/overlays.nix)
-  ];
-
-  nixpkgs = localLib.nixpkgs;
-  pkgs = nixpkgs { inherit overlays; };
-  drv = pkgs.haskellPackages.hpio;
-
-in
-if pkgs.lib.inNixShell then drv.env else drv
+(import ./default.nix {}).haskellPackages.hpioHlint.env
